aff32bd31935d75ef2fadc9057be57dc80953646
filipw <filip@strathweb.com>
2026-07-15 19:54:28 +0200

    emit schema when doing JSON output

--- build.js
+++ build.js
@@ -14,15 +14,22 @@ const plist = require("plist");
 const srcFile = path.join(__dirname, "src", "qsharp.tmLanguage.yml");
 const grammarsDir = path.join(__dirname, "grammars");
 
+// JSON Schema for TextMate grammars. Emitted only into the JSON outputs (as the
+// first key)
+const JSON_SCHEMA = "https://raw.githubusercontent.com/martinring/tmlanguage/master/tmlanguage.json";
+
 function build() {
     const text = fs.readFileSync(srcFile, "utf8");
     const grammar = jsYaml.load(text);
+
+    const jsonGrammar = { $schema: JSON_SCHEMA, ...grammar };
+    const jsonText = JSON.stringify(jsonGrammar, null, "\t") + "\n";
 
     fs.mkdirSync(grammarsDir, { recursive: true });
 
     fs.writeFileSync(
         path.join(grammarsDir, "qsharp.tmLanguage.json"),
-        JSON.stringify(grammar, null, "\t") + "\n"
+        jsonText
     );
 
     fs.writeFileSync(
@@ -38,7 +45,7 @@ function build() {
     if (fs.existsSync(testExtDir)) {
         fs.writeFileSync(
             path.join(testExtDir, "qsharp.tmLanguage.json"),
-            JSON.stringify(grammar, null, "\t") + "\n"
+            jsonText
         );
         console.log("Refreshed test-extension/qsharp.tmLanguage.json");
     }
